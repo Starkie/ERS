e5c2e0245734b139fb726fa15c3baf7fb6e95a62
Adriano Vega Llobell <adrianovega@hotmail.com>
2021-02-28 12:52:02 +0100

    Calcular la media y la mediana del sentimiento por estado

--- 3 - Twitter/USAsentimentAnalysis.py
+++ 3 - Twitter/USAsentimentAnalysis.py
@@ -1,16 +1,19 @@
 # !/usr/bin/env python3
 
-import json
-import csv
 import argparse
+import csv
+import json
+import statistics
 import pathlib
+
+from analysis.tweet_afinn import analyse_sentiment
 
 states_names = {'AL': 'Alabama', 'AK': 'Alaska', 'AZ': 'Arizona', 'AR': 'Arkansas', 'CA': 'California', 'CO': 'Colorado',
 'CT': 'Connecticut', 'DE': 'Delaware', 'FL': 'Florida', 'GA': 'Georgia', 'HI': 'Hawaii', 'ID': 'Idaho', 'IL': 'Illinois',
 'IN': 'Indiana', 'IA': 'Iowa', 'KS': 'Kansas', 'KY': 'Kentucky', 'LA': 'Louisiana', 'ME': 'Maine', 'MD': 'Maryland',
 'MA': 'Massachusetts', 'MI': 'Michigan', 'MN': 'Minnesota', 'MS': 'Mississippi', 'MO': 'Missouri', 'MT': 'Montana',
-'NE': 'Nebraska', 'NV': 'Nevada', 'NH': 'New Hampshire', 'NJ': 'New Jersey', 'NM': 'New Mexico', 'NY': 'New York', 
-'NC':'North Carolina', 'ND': 'North Dakota', 'OH': 'Ohio', 'OK': 'Oklahoma', 'OR': 'Oregon', 'PA': 'Pennsylvania', 
+'NE': 'Nebraska', 'NV': 'Nevada', 'NH': 'New Hampshire', 'NJ': 'New Jersey', 'NM': 'New Mexico', 'NY': 'New York',
+'NC':'North Carolina', 'ND': 'North Dakota', 'OH': 'Ohio', 'OK': 'Oklahoma', 'OR': 'Oregon', 'PA': 'Pennsylvania',
 'RI': 'Rhode Island', 'SC': 'South Carolina', 'SD': 'South Dakota', 'TN':'Tennessee', 'TX': 'Texas', 'UT': 'Utah',
 'VT': 'Vermont', 'VA': 'Virginia', 'WA': 'Washington','WV': 'West Virginia', 'WI': 'Wisconsin', 'WY': 'Wyoming'}
 
@@ -24,23 +27,26 @@ def isState(state):
     return state in states_names.keys()
 
 def readTweets(file):
+    sentiments_by_state = dict()
+
     with open(file, "r") as ins:
-        for line in ins:          
-            if (len(line)> 1): ## to avoid empty lines 
+        for line in ins:
+            if (len(line)> 1): ## to avoid empty lines
                 data = json.loads(line)
 
                 if "created_at" in data:
                     state = getState(data)
 
                     if isState(state) and "text" in data:
-                        print (data["text"])
-                        print (state)
+                        sentiment = analyse_sentiment(data["text"])
 
-def load_afinn():
-    afinn_path = pathlib.Path('./AFINN-111.txt').resolve()
+                        if sentiments_by_state.get(state) is None:
+                            sentiments_by_state[state] = []
 
-    with open(afinn_path, 'r') as afinn_file:
-        return dict(csv.reader(afinn_file, delimiter='\t'))
+                        sentiments_by_state[state].append(sentiment)
+
+    return sentiments_by_state
+
 
 # Declare the script parameters.
 parser = argparse.ArgumentParser(description='Analyses the sentiment on a given country from a collection of tweets.')
@@ -55,4 +61,7 @@ if not pathlib.Path(file).exists():
     raise FileNotFoundError(file)
 
 # Process the tweets.
-readTweets(file)
+sentiments_by_state = readTweets(file)
+
+mean_sentiment_by_state = {key: statistics.mean(value) for key, value in sentiments_by_state.items()}
+median_sentiment_by_state = {key: statistics.median(value) for key, value in sentiments_by_state.items()}
