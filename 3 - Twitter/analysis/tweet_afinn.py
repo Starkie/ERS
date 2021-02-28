30a74f88c1768edbb9fc5794598c5db719b1d06b
Adriano Vega Llobell <adrianovega@hotmail.com>
2021-02-28 11:31:10 +0100

    Crear módulo para ejecutar el AFINN

---
+++ 3 - Twitter/analysis/tweet_afinn.py
@@ -0,0 +1,44 @@
+# !/usr/bin/env python3
+
+import csv
+import os
+import pathlib
+import string
+
+# Load the AFINN file.
+def _load_afinn():
+    afinn_path = os.path.join(os.path.dirname(__file__), 'AFINN-111.txt')
+
+    afinn = dict()
+
+    with open(afinn_path, 'r') as afinn_file:
+        reader = csv.reader(afinn_file, delimiter='\t')
+
+        for row in reader:
+            afinn[row[0]] = int(row[1])
+
+    return afinn
+
+
+afinn = _load_afinn()
+
+# Analyse the sentiment of a given tweet.
+def analyse_sentiment(tweet):
+    clean_tweet = _remove_punctuation(tweet)
+
+    sentiment = 0
+
+    for word in clean_tweet.split():
+        lowerWord = word.lower()
+        sentiment += afinn.get(lowerWord, 0)
+
+    return sentiment
+
+# Remove the punctuation from the given string.
+def _remove_punctuation(content):
+    res = content
+
+    for char in '“”' + string.punctuation:
+        res = res.replace(char, "")
+
+    return res
