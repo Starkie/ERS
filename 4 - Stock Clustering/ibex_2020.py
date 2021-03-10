35ca03611b88fbe5128ae4c354b324a59f627881
Adriano Vega <adrianovega@hotmail.com>
2021-03-10 20:24:27 +0100

    Analizar el ratio de variación diaria

--- 4 - Stock Clustering/ibex_2020.py
+++ 4 - Stock Clustering/ibex_2020.py
@@ -11,4 +11,24 @@ def lista_google (dstart,dend):
      lis.append(f[0:]['Close'])
  return lis
 
+def analyse_variation (stocks):
+    stock_history = []
+
+    for stock in stocks:
+        stock_history.append(analyse_daily_variation(stock))
+
+    return stock_history
+
+def analyse_daily_variation(stock):
+    variation_ratio_histogram = [0]
+
+    for i in range(1,len(stock)):
+        ratio = (stock[i] - stock[i-1]) / 100
+        variation_ratio_histogram.append(ratio)
+
+    return variation_ratio_histogram
+
+
 lis=lista_google(datetime.datetime(2018, 1, 1),datetime.datetime(2019, 12, 31))
+
+analyse_variation(lis)
