2fd864ba3bc68f300b41af6cd606a25972124dde
Adriano Vega Llobell <adrianovega@hotmail.com>
2021-03-20 13:59:43 +0100

    Improve logging

--- 4 - LastFm/genius/genius_api.py
+++ 4 - LastFm/genius/genius_api.py
@@ -4,9 +4,11 @@ import lyricsgenius
 import os
 from time import sleep
 
-secrets = open(os.path.join(os.path.dirname(__file__), ".secrets"))
-api_key = secrets.read().strip()
-genius_API = lyricsgenius.Genius(api_key)
+# If no API key is provided, read it from the secrets store.
+_secrets = open(os.path.join(os.path.dirname(__file__), ".secrets"))
+_api_key = _secrets.read().strip()
+genius_API = lyricsgenius.Genius(_api_key)
+genius_API.verbose = False
 
 def lyrics_by_artists(songs_by_artist):
     lyrics_by_artist = []
@@ -20,6 +22,8 @@ def _get_artist_lyrics(artist_songs):
     artist_name = artist_songs['name']
     artist_lyrics = {"name": artist_name, "songs":dict()}
 
+    print(f"Searching lyrics of '{artist_name}'.")
+
     # Convert the songs to a set to a avoid duplication.
     for song in set(artist_songs['songs']):
         # Search by the artist and song to avoid incorrect results.
@@ -27,6 +31,8 @@ def _get_artist_lyrics(artist_songs):
 
         if found_song != None:
             artist_lyrics['songs'][song] = found_song.lyrics
+        else:
+            print(f"The song '{artist_name} - {song}' was not found.")
 
         # Add waits to ratelimit the requests.
         sleep(3)
