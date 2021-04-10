e0dc8c16f89112217269112ba45a0d325a3e58c6
Adriano Vega Llobell <adrianovega@hotmail.com>
2021-04-10 21:02:23 +0200

    Corregir nombres de las variables y añadir Summary al final

--- 5 - Graphs/SocialNetworkGraphs.py
+++ 5 - Graphs/SocialNetworkGraphs.py
@@ -1,33 +1,30 @@
 # !/usr/bin/env python3
 import snap
+import pprint
 
 from os import path
 
 graph_path = path.join(path.dirname(__file__), 'social_networks/links2.edges')
 
 ###########################################################################################
-# UNIDIRECTIONAL GRAPH
+# UNDIRECTED GRAPH
 ###########################################################################################
-(unidirectional_graph, ug_map) = snap.LoadEdgeListStr(snap.TUNGraph, graph_path, 0, 1, Mapping = True)
-ugraph_diameter = unidirectional_graph.GetBfsFullDiam(100, False)
+(undirected_graph, ug_map) = snap.LoadEdgeListStr(snap.TUNGraph, graph_path, 0, 1, Mapping = True)
+ugraph_diameter = undirected_graph.GetBfsFullDiam(100, False)
 
+undirected_degrees = undirected_graph.GetDegSeqV(False)
+undirected_degrees_dictionary = {id: undirected_degrees[id] for id in range(0, len(undirected_degrees))}
+undirected_degrees_sorted = sorted(undirected_degrees_dictionary.items(), key=lambda x: x[1], reverse=True)
 
-print(f"Diameter unidirectional: {ugraph_diameter}")
+top10_nodes_unidirect= []
 
-unidirectional_degrees = unidirectional_graph.GetDegSeqV(False)
-unidirectional_degrees_dictionary = {id: unidirectional_degrees[id] for id in range(0, len(unidirectional_degrees))}
-unidirectional_degrees_sorted = sorted(unidirectional_degrees_dictionary.items(), key=lambda x: x[1], reverse=True)
+for id, degrees in undirected_degrees_sorted[:10]:
+    top10_nodes_unidirect.append(f"Node {ug_map.GetKey(id)} has degree {degrees}")
 
-for id, degrees in unidirectional_degrees_sorted:
-    print("Node %s has degree %s" % (ug_map.GetKey(id), degrees))
+top_300_nodes_ids = [id for id, degrees in undirected_degrees_sorted[:300]]
 
-top_300_nodes_ids = [id for id, degrees in unidirectional_degrees_sorted[:300]]
-
-unidirectional_graph.DelNodes(top_300_nodes_ids)
-ugraph_diameter = unidirectional_graph.GetBfsFullDiam(100, False)
-
-print(f"Diameter unidirectional: {ugraph_diameter}")
-
+undirected_graph.DelNodes(top_300_nodes_ids)
+updated_ugraph_diameter = undirected_graph.GetBfsFullDiam(100, False)
 
 ###########################################################################################
 # DIRECTED GRAPH
@@ -36,20 +33,33 @@ print(f"Diameter unidirectional: {ugraph_diameter}")
 (directed_graph, g_map) = snap.LoadEdgeListStr(snap.TNGraph, graph_path, 0, 1, Mapping = True)
 graph_diameter = directed_graph.GetBfsFullDiam(100, True)
 
-print(f"Diameter directed: {graph_diameter}")
-
 in_degree, out_degree = directed_graph.GetDegSeqV(True)
 
 directed_graph_degrees = zip(range(0, len(in_degree)), in_degree, out_degree)
 directed_graph_degrees_sorted = sorted(directed_graph_degrees, key=lambda tup: tup[1], reverse=True)
 
-for node in directed_graph_degrees_sorted:
-  print("Node %s has in-degree %s and out-degree %s" % (ug_map.GetKey(node[0]), node[1], node[2]))
+top10_nodes_direct = []
+for node in directed_graph_degrees_sorted[:10]:
+  top10_nodes_direct.append(f"Node {ug_map.GetKey(node[0])} has in-degree {node[1]} and out-degree {node[2]}")
 
 top_300_in_ids = [id for id, _, _ in directed_graph_degrees_sorted[:300]]
 
 directed_graph.DelNodes(top_300_nodes_ids)
 
-graph_diameter = directed_graph.GetBfsFullDiam(100, True)
+updated_graph_diameter = directed_graph.GetBfsFullDiam(100, True)
 
-print(f"Diameter directed: {graph_diameter}")
+########################################################
+# SUMMARY
+########################################################
+
+print("SUMMARY:")
+print("--------------------------------------")
+print("UNDIRECTED GRAPH:")
+print("TOP 10 NODES:")
+pprint.pp(top10_nodes_unidirect)
+print(f"DIAMETER: Initial: {ugraph_diameter} Updated: {updated_ugraph_diameter}")
+print("--------------------------------------")
+print("DIRECTED GRAPH:")
+print("TOP 10 NODES:")
+pprint.pp(top10_nodes_direct)
+print(f"DIAMETER: Initial: {graph_diameter} Updated: {updated_graph_diameter}")
